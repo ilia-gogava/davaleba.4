db8c99313e105d3481585edc524a8016fd957109
iliaG <ilia.gogava141@gmail.com>
2026-02-07 22:46:41 +0400

    new

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+class Post(models.Model): # new
+    text = models.TextField()
+
+    def __str__(self): # new
+        return self.text[:50]
+
 # Create your models here.
+
